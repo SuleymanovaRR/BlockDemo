7c22115b66bbfbb790cc081f9f09ef32f5a5119d
Suleymanova RR <suleymanova.rr@yandex.ru>
2020-10-29 23:07:00 +0300

    Initial commit

---
+++ src/ru/test/Example.java
@@ -0,0 +1,8 @@
+package ru.test;
+
+public class Example {
+    //Выполнение программы на Java начинается с вызова метода main()
+    public static void main(String args[]) {
+        System.out.println("Java правит Интернетом!");
+    }
+}
---
+++ src/ru/test/Helloworld.java
@@ -0,0 +1,8 @@
+package ru.test;
+
+public class Helloworld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
